aa76ab72552bd458fc5e34f2e15f0ebef70bc529
Davron <davronbekq467@gmail.com>
2022-03-24 09:16:09 +0500

    Add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { LOGIN_ROUTE } from "../util/const";
+//
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const user = false;
   return (
-    <div className="navbar">
-      <h2>navbar</h2>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React
+          </Typography>
+          {user ? (
+            <Button color="inherit">Quit</Button>
+          ) : (
+            <NavLink to={LOGIN_ROUTE}>
+              <Button color="inherit">Login</Button>
+            </NavLink>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
