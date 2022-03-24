09dd0287b3646300c8dd6164311a2148eb82c214
Davron <davronbekq467@gmail.com>
2022-03-24 08:43:49 +0500

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppRouter from "./components/AppRouter";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
